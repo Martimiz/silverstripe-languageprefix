8fcc45de08a9513e77fadc1ab2effe2d5a2ac85d
martimiz <martine@balbus.nl>
2012-12-02 18:03:09 +0100

    Minor: commented out the custom prefix example in _config.php

--- _config.php
+++ _config.php
@@ -3,8 +3,10 @@
 Object::add_extension('Page', 'LanguagePrefix'); 
 Object::add_extension('Page_Controller', 'LanguagePrefix_Controller'); 
 
-// Format: locale => prefix
+
+/*
 LanguagePrefix::$language_prefixes = array(
     	'en_US' => 'en',
     	'nl_NL' => 'nl'
 );
+ */
